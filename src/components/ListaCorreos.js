d8691d1e9a58bbcaa483f32ea09b2d3cdc099fb8
Luis Cascante <luisalonzocascante@gmail.com>
2023-10-28 08:57:59 -0600

    pasando lista como prop

--- src/components/ListaCorreos.js
+++ src/components/ListaCorreos.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-export const ListaCorreos = (props) => {
-  console.log(props);
-  return <div>ListaCorreos</div>;
+export const ListaCorreos = ({ listaCorreos }) => {
+  return (
+    <ul>
+      {listaCorreos.map((correo) => (
+        <li key={correo.email}>
+          <p>{correo.email}</p>
+          <p>{correo.password}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
